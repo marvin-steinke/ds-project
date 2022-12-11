eeac7b5b0c08aceeca63c35c3988d64a6d6a9b6f
marvin.steinke <marvin.steinke@campus.tu-berlin.de>
2022-12-11 10:58:11 +0100

    added generic class SingleModelSimulator, refactored all simulators by subclassing SingleModelSimulator

--- src/simulators/simple_battery_simulator.py
+++ src/simulators/simple_battery_simulator.py
@@ -8,6 +8,7 @@ Notes: *discharge_s* is specified in Ws since the consumption is modeled per sec
 
 import mosaik_api
 from models.simple_battery_model import SimpleBatteryModel # type: ignore
+from utils.single_model_simulator import SingleModelSimulator # type: ignore
 
 META = {
     'type': 'event-based',
@@ -20,29 +21,9 @@ META = {
     },
 }
 
-class SimpleBatterySim(mosaik_api.Simulator):
+class SimpleBatterySim(SingleModelSimulator):
     def __init__(self):
-        super().__init__(META)
-        self.eid_prefix = 'Battery_'
-        self.entities = {}  # Maps EIDs to model instances/entities
-        self.time = 0
-
-    def init(self, sid, time_resolution, eid_prefix=None):
-        if float(time_resolution) != 1.:
-            raise ValueError(f'SimpleBatterySim only supports time_resolution=1., but {time_resolution} was set.')
-        if eid_prefix is not None:
-            self.eid_prefix = eid_prefix
-        return self.meta
-
-    def create(self, num, model, capacity = 100, charge = -1):
-        next_eid = len(self.entities)
-        entities = []
-        for i in range(next_eid, next_eid + num):
-            model_instance = SimpleBatteryModel(capacity, charge)
-            eid = self.eid_prefix + str(i)
-            self.entities[eid] = model_instance
-            entities.append({'eid': eid, 'type': model})
-        return entities
+        super().__init__(META, SimpleBatteryModel)
 
     def step(self, time, inputs, max_advance):
         self.time = time
@@ -56,18 +37,6 @@ class SimpleBatterySim(mosaik_api.Simulator):
                 model_instance.discharge_s = new_discharge_s
             model_instance.step()
         return None
-
-    def get_data(self, outputs):
-        data = {}
-        for eid, attrs in outputs.items():
-            model = self.entities[eid]
-            data['time'] = self.time
-            data[eid] = {}
-            for attr in attrs:
-                if attr not in self.meta['models']['SimpleBatteryModel']['attrs']: # type: ignore
-                    raise ValueError(f'Unknown output attribute: {attr}')
-                data[eid][attr] = getattr(model, attr)
-        return data
 
 def main():
     return mosaik_api.start_simulation(SimpleBatterySim())
