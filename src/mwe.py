426e1d31eb3ece1bf8572466ed4df4d8ec1a2010
Henrik <henrik.nickel@campus.tu-berlin.de>
2023-02-04 15:03:32 +0100

    removed git typo

--- src/mwe.py
+++ src/mwe.py
@@ -25,13 +25,8 @@ SIM_CONFIG = {
         },
 }
 
-<<<<<<< HEAD
-START = '2014-01-01 00:00:00'
-#CONSUMPTION_DATA = '../resources/consumption.csv'
-=======
 START = '2014-01-01 14:08:00'
 CONSUMPTION_DATA = '../resources/consumption.csv'
->>>>>>> a7c8342 (adjusted data for presentation)
 PV_DATA = '../resources/testing_PV.csv'
 CARBON_DATA = '../resources/testing_carbon.csv'
 END = 3000
